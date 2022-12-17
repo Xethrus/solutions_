10e251b5b349ab2d33a7393e05b100c420af2eec
xethrus <xethrus@gmail.com>
2022-12-16 16:30:22 -0800

    garbage

---
+++ chapter_3/question6.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+class Node {
+public:
+  int data;
+  Node* below;
+  Node() { data = 0; below = NULL; }
+  Node(int data) {
+    this->data = data;
+    this->below = NULL;
+  }
+};
+
+class Stack {
+  Node* top;
+public:
+  Stack() { top = nullptr; }
+  void push(int);
+  int pop();
+  int peek();
+  bool isEmpty();
+  void sortStack();
+};
+
+void Stack::push(int data) {
+  Node* newNode = new Node(data);
+  if (top == NULL) {
+    top = newNode; 
+  }
+  newNode->below = top;
+  top = newNode;
+  return;
+}
+
+int Stack::pop() {
+  if (top == NULL) {
+    std::cerr << "FAILURE NO NODE FOUND" << std::endl;
+    return -1;
+  } else {
+    Node* temp = top;
+    top = top->below;
+    return temp->data;
+  }
+}
+
+int Stack::peek() {
+  if (top == NULL) {
+    std::cerr << "NO CONTENT TO PEEK" << std::endl;
+  } else {
+    return top->data;
+  }
+}
+
+bool Stack::isEmpty() {
+  if (top == NULL) {
+    return true; 
+  } else {
+    return false;
+  }
+}
+//O n^2 
+//infinite loop lol
+void Stack::sortStack() {
+  Stack bufferStack;
+  int tempValue;
+  tempValue = this->peek();
+  bufferStack.push(this->pop());
+  std::cout << "hello1" << std::endl;
+  while(!this->isEmpty()) {
+    std::cout << "hello2" << std::endl;
+    tempValue = this->peek();
+    while(bufferStack.peek() < tempValue) {
+      std::cout << bufferStack.peek() << std::endl;
+      this->push(bufferStack.pop());
+    }
+    bufferStack.push(tempValue);
+  }
+  while(!bufferStack.isEmpty()) {
+    this->push(bufferStack.pop());
+  }
+}
+
+int getSmaller(int a, int b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+int main() {
+  Stack stack;
+  stack.push(1);
+  stack.push(3);
+  stack.push(5);
+  stack.push(5);
+  stack.push(1);
+  stack.push(4);
+  stack.sortStack();
+  return 0;
+}
