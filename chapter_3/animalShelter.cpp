5d0ffcdb4b4f4ae7d9d082fc39e6eca94890ad6f
xethrus <xethrus@gmail.com>
2022-12-29 11:18:25 -0800

    starting to work great

--- chapter_3/animalShelter.cpp
+++ chapter_3/animalShelter.cpp
@@ -45,24 +45,62 @@ std::string Queue::dequeue() {
   } else {
     std::string species = this->rear->catOrDog;
     this->rear = this->rear->next;
-    /*
-    std::cout << this->front->next->catOrDog << std::endl;
-    this->front = this->front->next;
-    */
     return species;
   }
 }
 
+class AnimalShelter {
+public:
+  Queue dogs;
+  Queue cats;
+  Queue both;
+  
+  void enqueueCat(std::string);
+  void enqueueDog(std::string);
+  std::string dequeueCat();
+  std::string dequeueDog();
+  std::string dequeueFirst();
+};
+
+void AnimalShelter::enqueueCat(std::string name) {
+  this->cats.enqueue(name);
+  this->both.enqueue(name);
+}
+
+void AnimalShelter::enqueueDog(std::string name) {
+  this->dogs.enqueue(name);
+  this->both.enqueue(name);
+}
+
+std::string AnimalShelter::dequeueCat() {
+  std::string animal = this->cats.dequeue();
+  std::cout << "==== " << animal << " ==== Has been adopted"
+    << std::endl;
+  return animal;
+}
+
+std::string AnimalShelter::dequeueDog() {
+  std::string animal = this->dogs.dequeue();
+  std::cout << "==== " << animal << " ==== Has been adopted"
+    << std::endl;
+  return animal;
+}
+
+std::string AnimalShelter::dequeueFirst() {
+  std::string animal = this->both.dequeue();
+  std::cout << "==== " << animal << " ==== Has been adopted"
+    << std::endl;
+  return animal;
+}
+
+
+
 int main() {
-  Queue test;
-  test.enqueue("1");
-  test.enqueue("2");
-  test.enqueue("3");
-  test.enqueue("4");
-  test.enqueue("5");
-  std::cout << "removing from beginning of queue: " <<
-    test.dequeue() << std::endl;
-  std::cout << "rear: " << test.rear->catOrDog << std::endl;
-  std::cout << "front: " << test.front->catOrDog << std::endl;
+  AnimalShelter shelter;
+  shelter.enqueueDog("moongman");
+  shelter.enqueueCat("clarence");
+  shelter.enqueueCat("clarence");
+  shelter.dequeueCat();
+  shelter.dequeueFirst();
   return 0;
 }
