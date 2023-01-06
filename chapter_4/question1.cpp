c2cce6e46c5c0e96fa2c583b6249c65eb74d2f22
xethrus <xethrus@gmail.com>
2023-01-06 10:25:31 -0800

    ideas explored

--- chapter_4/question1.cpp
+++ chapter_4/question1.cpp
@@ -1,76 +1,110 @@
-#include <iostream>
+include <iostream>
 
 class Node {
-public:
   int data;
-  Node* left;
-  Node* right;
-  int height;
-  Node(int data, Node* l, Node* r, int h) : data(data), left(l), right(r), height(h) {}
+  Node* left_child;
+  Node* right_child;
+  Node(int data) : data(data), left_child(nullptr), right_child(nullptr) {}
 };
 
+class BinaryTree {
+  Node* root;
+  BinaryTree() : root(nullptr) {}
 
-
-int height(Node* node) {
-  if(node == nullptr) {
-    return 0;
+  void insertNode(int);
+  int getLeftHeight();
+  int getRightHeight();
+};
+int BinaryTree::getRightHeight() {
+  Node* cursor = root;
+  int count = 1;
+  if(cursor->right_child) {
+    cursor = cursor->right_child;
+    while(cursor->right_child || cursor->right_child) {  
+      if(cursor->right_child) {
+        cursor = cursor->right_child;
+      } else {
+        cursor = cursor->left_child;
+      }
+      count++;
+    }
   }
-  return node->height;
+  return count;
 }
-
-void updateHeight(Node* node) {
-  node->height = 1 + std::max(height(node->left), height(node->right));
-}
-
-Node* rotateLeft(Node* node) {
-  Node* right = node->right;
-  node->right = right->left;
-  right->left = node;
-  updateHeight(node);
-  updateHeight(right);
-  return right;
-}
-
-Node* rotateRight(Node* node) {
-  Node* left = node->left;
-  node->left = left->right;
-  left->right = node;
-  updateHeight(node);
-  updateHeight(left);
-  return left;
-}
-
-int balanceFactor(Node* node) {
-  return height(node->right) - height(node->left);
-}
-
-Node* insert(Node* node, int data) {
-  if(node == nullptr) {
-    return new Node(data, nullptr, nullptr, 1);
+int BinaryTree::getLeftHeight() {
+  Node* cursor = root;
+  int count = 1;
+  if(cursor->left_child) {
+    cursor = cursor->left_child;
+    while(cursor->right_child || cursor->left_child) {
+      if(cursor->right_child) {
+        cursor = cursor->right_child;
+      } else {
+        cursor = cursor->left_child;
+      }
+      count++;
+    }
   }
-
-  if (data < node->data) {
-    node->left = insert(node->left, data);
+  return count;
+}
+void BinaryTree::insertNode(int data) {
+  Node* newNode = new Node(data);
+  Node* cursor = root;
+  if(!root) {
+    root = new Node(data);
   } else {
-    node->right = insert(node->right, data);
-  }
-
-  updateHeight(node);
-
-  int balance = balanceFactor(node);
-  if (balance == 2) {
-    if (balanceFactor(node->right) < 0) {
-      node->right = rotateRight(node->right);
+    if(this.getLeftHeight() > this.getRightHeight()) {
+      //left heigh is greater
+      //add to right
+      if(cursor->right_child) {
+        cursor = cursor->right_child;
+        while(cursor->right_child || cursor->left_child) {
+          if(cursor->right_child) {
+            cursor = cursor->right_child;  
+          }
+          if(cursor->left_child) {
+            cursor = cursor->right_child;
+          }
+        } else {
+          if(!cursor->left_child) {
+            cursor->left_child = newNode;
+            return;
+          }
+          if(!cursor->right_child) {
+            cursor->right_child = newNode;
+            return;
+          }
+        }
+      } else {
+        cursor->right_child = newNode;
+        return;
+      }
+    } else {
+      if(cursor->left_child) {
+        cursor = cursor->left_child;
+        while(cursor->right_child || cursor->left_child) {
+          if(cursor->right_child) {
+            cursor = cursor->right_child;  
+          }
+          if(cursor->left_child) {
+            cursor = cursor->right_child;
+          }
+        } else {
+          if(!cursor->left_child) {
+            cursor->left_child = newnode;
+            return;
+          }
+          if(!cursor->right_child) {
+            cursor->right_child = newnode;
+            return;
+          }
+        }
+      } else {
+        cursor->left_child = newnode;
+        return;
+      }
     }
-    return rotateLeft(node);
   }
-  if (balance == -2) {
-    if (balanceFactor(node->left) > 0) {
-      node->left = rotateLeft(node->left);
-    }
-    return rotateRight(node);
-  }
-  return node;
 }
 
 int main() {
